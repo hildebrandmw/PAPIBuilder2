faf1a9cc15dca59f0ec1715a86bc74d21d6942a8
Mark Hildebrand <mhildebrand@ucdavis.edu>
2018-12-09 09:29:42 -0800

    Add perfmon2 examples for getting codes.

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -3,20 +3,27 @@
 using BinaryBuilder
 
 name = "papi"
-version = v"0.1.0"
+version = v"0.2.0"
 
 # Collection of sources required to build papi
 sources = [
+    "https://sourceforge.net/projects/perfmon2/files/libpfm4/libpfm-4.10.1.tar.gz" =>
+    "c61c575378b5c17ccfc5806761e4038828610de76e2e34fac9f7fa73ba844b49",
+
     "https://bitbucket.org/icl/papi.git" =>
-    "0fdac4fc7f95f0ac8039e431419a5133088911af" #master
-    #"b2d6558c481391f15000ae023c5dc4fee73dc4e6", # stable-5.5.1
+    "0fdac4fc7f95f0ac8039e431419a5133088911af",
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
-cd papi/src
-./configure --prefix=$prefix --host=$target --enable-perfevent-rdpmc=no
+cd libpfm-4.10.1/
+make -j${nprocs}
+make PREFIX=$prefix install
+make PREFIX=$prefix install_examples
+
+cd ../papi/src
+./configure --prefix=$prefix --host=$host --enable-perfevent-rdpmc=no
 make -j${nprocs}
 make install
 exit
@@ -32,7 +39,9 @@ platforms = [
 # The products that we will ensure are always built
 products(prefix) = [
     LibraryProduct(prefix, "libpapi", :libpapi),
-    LibraryProduct(prefix, "libpfm", :libpfm)
+    LibraryProduct(prefix, "libpfm", :libpfm),
+    ExecutableProduct(prefix, "check_events", :check_events),
+    ExecutableProduct(prefix, "showevtinfo", :showevtinfo),
 ]
 
 # Dependencies that must be installed before this package can be built
